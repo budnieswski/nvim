ef63a93324d3d496962c7f845ef9a6ea6177885f
Budnieswski <budnieswski@gmail.com>
2024-03-07 15:39:05 -0300

    feat: add some configs

--- lua/configs.lua
+++ lua/configs.lua
@@ -4,13 +4,18 @@ vim.g.mapleader = " "
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
+vim.autoindent = true
+
+-- search
+vim.ignorecase = true
+vim.smartcase = true
 
 -- cursor & lines
 vim.opt.cursorline = true -- highlight the current line
 vim.opt.number = true
 vim.opt.numberwidth = 4
 vim.opt.relativenumber = true
-vim.opt.scrolloff = 5 -- bottom padding when scrolling
+vim.opt.scrolloff = 10-- bottom padding when scrolling
 
 vim.opt.showcmd = false
 vim.opt.backup = false
