9a546702f45e397800facf95c001d2cacef15d81
Budnieswski <budnieswski@gmail.com>
2024-03-19 20:58:22 -0300

    feat: add trouble

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,29 +1,28 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  --config = function()
-  --  vim.keymap.set('n', '<leader>b', ":Neotree toggle<CR>", {})
-  --end
-  keys = {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	--config = function()
+	--  vim.keymap.set('n', '<leader>b', ":Neotree toggle<CR>", {})
+	--end
+	keys = {
 		{
-			'<leader>fe',
+			"<leader>fe",
 			function()
-				require('neo-tree.command').execute({ toggle = true, dir = vim.loop.cwd() })
+				require("neo-tree.command").execute({ toggle = true, reveal_force_cwd = true })
 			end,
-			desc = 'Explorer NeoTree (cwd)',
+			desc = "Explorer NeoTree (cwd)",
 		},
-	--	{ '<leader>e', '<leader>fe', desc = 'Explorer NeoTree (cwd)', remap = true },
 		{
-			'<leader>be',
+			"<leader>be",
 			function()
-				require('neo-tree.command').execute({ source = 'buffers', toggle = true })
+				require("neo-tree.command").execute({ source = "buffers", toggle = true })
 			end,
-			desc = 'Buffer explorer',
-		}
-	}
+			desc = "Buffer explorer",
+		},
+	},
 }
