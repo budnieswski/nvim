900bc37df20f2cbbebc6a8f8f2d90bbbe45d6953
Budnieswski <budnieswski@gmail.com>
2024-03-27 12:21:41 -0300

    feat: add autpairs and autotag

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,32 +1,33 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "gopls", "golangci_lint_ls", "tsserver" }
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lsp = require("lspconfig")
-      lsp.lua_ls.setup({})
-      lsp.gopls.setup({})
-      -- lsp.golangci_lint_ls.setup({})
-      lsp.tsserver.setup({})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "gopls", "golangci_lint_ls", "tsserver", "tailwindcss" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lsp = require("lspconfig")
+			lsp.lua_ls.setup({})
+			lsp.gopls.setup({})
+			-- lsp.golangci_lint_ls.setup({})
+			lsp.tsserver.setup({})
+			lsp.tailwindcss.setup({})
 
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-    end
-  },
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+		end,
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,14 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	dependencies = { "windwp/nvim-ts-autotag" },
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = { "lua", "vim", "go", "javascript", "typescript", "tsx", "html", "make", "yaml" },
 			auto_install = true,
+			autotag = {
+				enable = true,
+			},
 			highlight = { enable = true },
 			indent = { enable = true },
 			incremental_selection = {
